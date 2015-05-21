0d0d9978cb296a12ed963a386dac32ab79180417
argo49 <argo_49@hotmail.com>
2015-05-20 23:15:59 -0700

    Made a more general function to serve html and ejs files

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ var app = require('../app');
 var routes = {};
 
 // Path to distribution html, js and css
-var distPath = './views/dist/';
+// Always start with ./views/ until find a way to change it in ejs
+var distRoot = './views/'          // EJs views root
+var distPath = distRoot + 'dist/'; // Path to distribution html, js and css
 
 // GET home page
 routes.index = function (req, res) {
-    res.sendfile(distPath + 'index.html');
+    respondWith(res, 'index.html');
+    //res.sendfile(distPath + 'index.html');
 };
 
 // GET normalize.css
@@ -17,15 +20,28 @@ routes.normalizecss = function (req, res) {
 
 // GET 404 page
 routes.fileNotFound = function (req, res) {
-    res.render('dist/status/404.ejs');
+    console.log("404");
+    respondWith(res, 'status/404.ejs');
+    //res.render('dist/status/404.ejs');
 };
 
 /**
- * [getPath description]
- * @return {[type]} [description]
+ * filepath is assumed to start at distPath
  */
-function getPath () {
+function respondWith (res, filepath, data) {
+    if (isHTML(filepath)) {
+        res.sendfile(distPath + filepath);
+    } else {
+        var pathWithRemovedEjsRoot = distPath.slice(distRoot.length, distPath.length);
+        var folderPath = pathWithRemovedEjsRoot + filepath;
 
+        data = data || {};
+        res.render(folderPath, data);
+    }
+}
+
+function isHTML (filepath) {
+    return /\w+\.html$/.test(filepath);
 }
 
 module.exports = routes;
